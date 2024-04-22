dff31fb7a6a7af5403a58e5636c981237ffbc273
trettifjerde <sasjandreeva@gmail.com>
2024-04-22 20:53:42 +0200

    sanity types

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { eventType } from './schemaTypes/eventType'
+import { venueType } from './schemaTypes/venueType'
+import { artistType } from './schemaTypes/artistType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [eventType, venueType, artistType],
 }
